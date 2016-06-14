15587bece9bbd1f6243e152f004140007eb8c01a
Rafael Fourquet <fourquet.rafael@gmail.com>
2016-06-14 10:31:21 +0530

    implement copy and == for MersenneTwister (fix #15698)

--- test/random.jl
+++ test/random.jl
@@ -410,3 +410,25 @@ let mta = MersenneTwister(seed), mtb = MersenneTwister(seed)
         @test rand(mtb, Float64) == tmp[j]
     end
 end
+
+# copy and ==
+let seed = rand(UInt32, 10)
+    r = MersenneTwister(seed)
+    @test r == MersenneTwister(seed) # r.vals should be all zeros
+    s = copy(r)
+    @test s == r && s !== r
+    skip, len = rand(0:2000, 2)
+    for j=1:skip
+        rand(r)
+        rand(s)
+    end
+    @test rand(r, len) == rand(s, len)
+    @test s == r
+end
+
+# intialization
+@test_throws DomainError Base.dSFMT.DSFMT_state(zeros(Int32, rand(0:Base.dSFMT.JN32-1)))
+@test_throws DomainError MersenneTwister(zeros(UInt32, 1), Base.dSFMT.DSFMT_state(),
+                                         zeros(Float64, 10), 0)
+@test_throws DomainError MersenneTwister(zeros(UInt32, 1), Base.dSFMT.DSFMT_state(),
+                                         zeros(Float64, Base.Random.MTCacheLength), -1)
